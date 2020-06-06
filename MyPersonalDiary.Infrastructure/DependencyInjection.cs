2d1057ca94b4a7ccceaa85ee0e04d5ec75d88539
Sahil <me.sahilverma@hotmail.com>
2020-06-06 15:31:11 +0530

    Add project files.

---
+++ MyPersonalDiary.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyPersonalDiary.Infrastructure.Data;
+using MyPersonalDiary.Infrastructure.Models;
+
+namespace MyPersonalDiary.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("SqlDbConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            services.AddDefaultIdentity<ApplicationUser>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+
+            return services;
+        }
+    }
+}
